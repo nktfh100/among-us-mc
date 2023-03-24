a201a20664988fb6a9468b48eb89374c5f0f024b
Luncaaa <luca.alavoir@gmail.com>
2023-03-24 18:35:09 +0100

    Add new enums for the AUArenaEnd event

---
+++ src/main/java/com/nktfh100/AmongUs/enums/GameEndReasons.java
@@ -0,0 +1,5 @@
+package com.nktfh100.AmongUs.enums;
+
+public enum GameEndReasons {
+    IMPOSTERS_WIN, CREWMATES_WIN, PLUGIN_RELOAD, COMMAND,
+}
---
+++ src/main/java/com/nktfh100/AmongUs/enums/GameEndWinners.java
@@ -0,0 +1,5 @@
+package com.nktfh100.AmongUs.enums;
+
+public enum GameEndWinners {
+    CREWMATES, IMPOSTERS, NOBODY
+}
