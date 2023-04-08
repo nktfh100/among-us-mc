8d55ea89c2a26abc7ab5c5bf1cd212284d542a6d
Luncaaa <luca.alavoir@gmail.com>
2023-04-08 18:39:27 +0200

    Fix not being able to use the arena selector item

--- src/main/java/com/nktfh100/AmongUs/events/BlockPlace.java
+++ src/main/java/com/nktfh100/AmongUs/events/BlockPlace.java
@@ -16,5 +16,14 @@ public class BlockPlace implements Listener {
 		if (pInfo.getIsIngame()) {
 			ev.setCancelled(true);
 		}
+
+		if (ev.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(Main.getItemsManager().getItem("arenasSelector").getItem().getTitle())) {
+			ev.setCancelled(true);
+			if (Main.getConfigManager().getBungeecord() && Main.getConfigManager().getBungeecordIsLobby()) {
+				Main.getBungeArenaManager().openArenaSelector(pInfo);
+			} else if (!Main.getConfigManager().getBungeecord()) {
+				Main.getArenaManager().openArenaSelector(pInfo);
+			}
+		}
 	}
 }
